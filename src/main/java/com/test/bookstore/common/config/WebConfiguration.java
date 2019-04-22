e14024ea00fbc99a789e80536a426e30b52206df
Supanuth Ongsuk <n.nsupanuth@gmail.com>
2019-04-22 20:35:57 +0700

    Add web configuration

---
+++ src/main/java/com/test/bookstore/common/config/WebConfiguration.java
@@ -0,0 +1,29 @@
+package com.test.bookstore.common.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.zalando.problem.ProblemModule;
+import org.zalando.problem.violations.ConstraintViolationProblemModule;
+
+import java.util.List;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.test.bookstore.controller"})
+public class WebConfiguration implements WebMvcConfigurer{
+
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        ObjectMapper mapper = Jackson2ObjectMapperBuilder.json()
+            .modules(new ProblemModule(), new ConstraintViolationProblemModule()).build();
+
+        converters.add(new MappingJackson2HttpMessageConverter(mapper));
+    }
+
+}
+
